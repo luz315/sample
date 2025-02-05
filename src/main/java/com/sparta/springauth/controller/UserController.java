7c96aae7e7d96a83cbf793abeadb99f8afb7986c
poorm <315luz10@gmail.com>
2025-02-06 01:07:17 +0900

    [feat] 회원 가입 및 로그인 구현

---
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.sparta.springauth.controller;
+
+import com.sparta.springauth.dto.LoginRequestDto;
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto) {
+        userService.signup(requestDto);
+        return "redirect:/api/user/login-page";
+        //리다이렉트를 안 쓰고 그냥 "login"을 반환하면, 현재 URL이 /api/user/signup인 상태에서 login.html이 보이게 됨.
+    }
+
+    @PostMapping("/user/login")
+    public String signup(LoginRequestDto requestDto, HttpServletResponse res) {
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error";
+        }
+
+        return "redirect:/";
+    }
+
+
+}
